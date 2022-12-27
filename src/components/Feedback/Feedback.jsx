6ca65eb8ae1f00d5628d39c235fae3a777fded9d
Ihor Smyk <ismyk256@gmail.com>
2022-12-27 23:14:07 +0200

    upd

--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -1,29 +1,63 @@
 import { Component } from 'react';
 import c from './Feedback.module.css';
+import { PropTypes } from 'prop-types';
 
 export class Feedback extends Component {
   state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
+    good: this.props.defaultValueGood || 0,
+    neutral: this.props.defaultValueNeutral || 0,
+    bad: this.props.defaultValueBad || 0,
   };
 
+
+  handleGoodFeedbackIncrement = e => {
+    // let good = this.state.good;
+    this.setState({ good: 1 });
+    // console.log(e);
+  };
+
+  handleNeutralFeedbackIncrement = () => {};
+
+  handleBadFeedbackIncrement = () => {};
+
   render() {
+    console.log("state->", this.state.good);
+    console.log("good->", this.good);
     return (
       <form className={c.form} action="submit">
         <p className={c.message}>Please leave feedback</p>
         <ul className={c.btnList}>
-          <li>
-            <button className={c.btnGood}>Good</button>
+          <li className={c.item}> 
+            <button
+              className={c.btnGood}
+              type="button"
+              onClick={this.handleGoodFeedbackIncrement}
+            >
+              Good
+            </button>
           </li>
-          <li>
-            <button className={c.btnNeutral}>Neutral</button>
+          <li className={c.item}>
+            <button
+              className={c.btnNeutral}
+              type="button"
+              onClick={this.handleNeutralFeedbackIncrement}
+            >
+              Neutral
+            </button>
           </li>
-          <li>
-            <button className={c.btnBad}>Bad</button>
+          <li className={c.item}>
+            <button
+              className={c.btnBad}
+              type="button"
+              onClick={this.handleBadFeedbackIncrement}
+            >
+              Bad
+            </button>
           </li>
         </ul>
       </form>
     );
   }
 }
+
+Feedback.propTypes = {};
